0ffa7ac7993c1cac9513d778693b6ac35c19672d
Armando <armando@hibrido.com.br>
2019-11-16 10:26:09 -0300

    configura styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import GlobalStyle from "./styles/global";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">dwadaw</div>
+    </>
   );
 }
 
